e11935218cebbf1993ccd6c8e2c2a6fa6ec53908
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-08-13 10:51:25 -0400

    Update 73_Set matrix zeros.cpp

--- 73_Set matrix zeros.cpp
+++ 73_Set matrix zeros.cpp
@@ -3,6 +3,40 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        vector<bool> rowZero(m);
+        vector<bool> colZero(n);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; ++i) 
+            if (rowZero[i]) 
+                for (int j = 0; j < n; ++j)
+                    matrix[i][j] = 0;
+        for (int j = 0; j < n; ++j) 
+            if (colZero[j])
+                for (int i = 0; i < m; ++i)
+                    matrix[i][j] = 0;
+    }
+};
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
         vector<bool> zero_cols(n, false);
         for (int i = 0; i < m; i++) {
             bool zero = false;
