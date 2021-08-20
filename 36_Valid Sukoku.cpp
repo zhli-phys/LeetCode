50bd7cd33d9ba0005ff5694ea250f56fe3e9d454
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-08-20 13:03:45 -0400

    codes

--- 36_Valid Sukoku.cpp
+++ 36_Valid Sukoku.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> rows(9, vector<bool>(9, false));
+        vector<vector<bool>> cols(9, vector<bool>(9, false));
+        vector<vector<bool>> blocks(9, vector<bool>(9, false));
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.')
+                    continue;
+                int num = board[i][j] - '1';
+                if (rows[i][num])
+                    return false;
+                rows[i][num] = true;
+                if (cols[j][num])
+                    return false;
+                cols[j][num] = true;
+                int block = (i / 3) * 3 + (j / 3);
+                if (blocks[block][num])
+                    return false;
+                blocks[block][num] = true;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
         for (int i = 0; i < 9; i++) {
             if (!helper(board[i]))
                 return false;
