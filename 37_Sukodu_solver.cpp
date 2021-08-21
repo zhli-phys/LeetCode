dd9464032f5105391fc4dadfb789ae66ed715986
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-08-21 13:27:35 -0400

    codes

--- 37_Sukodu_solver.cpp
+++ 37_Sukodu_solver.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        vector<vector<bool>> filled(27, vector<bool>(9));
+        vector<vector<bool>> used(27, vector<bool>(9));
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
                 if (board[i][j] != '.') {
                     int num = board[i][j] - '1';
-                    filled[i][num] = true;
-                    filled[j+9][num] = true;
-                    filled[3*(i/3)+(j/3)+18][num] = true;
+                    used[i][num] = true;
+                    used[j+9][num] = true;
+                    used[3*(i/3)+j/3+18][num] = true;
                 }
             }
         }
-        vector<int> path;
-        dfs(0, board, path, filled);
+        dfs(0, board, used);
     }
     
-    bool dfs(int pos, vector<vector<char>>& board, vector<int>& path, vector<vector<bool>>& filled) {
-        if (pos == 81)
+    bool dfs(int pos, vector<vector<char>>& board, vector<vector<bool>>& used) {
+        if (pos >= 81)
             return true;
-        int row = pos / 9;
-        int col = pos % 9;
-        int block = 3*(row/3)+(col/3);
-        if (board[row][col] != '.')
-            return dfs(pos + 1, board, path, filled);
-        for (int curr = 0; curr < 9; ++curr) {
-            if (filled[row][curr] || filled[9+col][curr] || filled[18+block][curr])
+        int i = pos % 9;
+        int j = pos / 9;
+        if (board[i][j] != '.')
+            return dfs(pos+1, board, used);
+        int block = 3 * (i / 3) + j / 3;
+        for (int num = 0; num < 9; ++num) {
+            if (used[i][num] || used[j+9][num] || used[block+18][num])
                 continue;
-            path.push_back(row*81+col*9+curr);
-            filled[row][curr] = true;
-            filled[9+col][curr] = true;
-            filled[18+block][curr] = true;
-            board[row][col] = '1' + curr;
-            if (dfs(pos+1, board, path, filled))
+            board[i][j] = '1' + num;
+            used[i][num] = true;
+            used[j+9][num] = true;
+            used[block+18][num] = true;
+            if (dfs(pos+1, board, used))
                 return true;
-            board[row][col] = '.';
-            filled[row][curr] = false;
-            filled[9+col][curr] = false;
-            filled[18+block][curr] = false;
-            path.pop_back();
+            used[block+18][num] = false;
+            used[j+9][num] = false;
+            used[i][num] = false;
+            board[i][j] = '.';
         }
         return false;
     }
