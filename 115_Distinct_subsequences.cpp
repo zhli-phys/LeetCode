e319f463f07b688a9371bea41b8d03d4ce0b88ce
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-09-19 03:27:14 -0400

    codes

---
+++ 115_Distinct_subsequences.cpp
@@ -0,0 +1,21 @@
+using ull = unsigned long long;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        vector<vector<ull>> dp(m + 1, vector<ull>(n + 1));
+        dp[0][0] = 1;
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= n; ++j) {
+                dp[i][j] = dp[i-1][j];
+                if (s[i-1] == t[j-1]) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+        return dp.back().back();
+    }
+};
