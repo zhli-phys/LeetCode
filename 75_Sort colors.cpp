b55539bbd8f82a31d837ec0ceaf6fc91bbb2bbbb
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-10-27 04:21:08 -0400

    Update 75_Sort colors.cpp

--- 75_Sort colors.cpp
+++ 75_Sort colors.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int zero = 0;
+        int one = 0;
+        int two = n - 1;
+        while (one <= two) {
+            if (nums[one] == 0) {
+                swap(nums[one], nums[zero]);
+                ++zero;
+                ++one;
+            }
+            else if (nums[one] == 2) {
+                swap(nums[one], nums[two]);
+                --two;
+            }
+            else
+                ++one;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
         int right = nums.size() - 1;
         int left = 0;
         while (right >= 0 && nums[right] == 2) 
