78c813e30edd0f5f10a1be96da2ce8e05d361b7b
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-11-26 02:38:01 -0500

    Update 35_Search_insert_position.cpp

--- 35_Search_insert_position.cpp
+++ 35_Search_insert_position.cpp
@@ -6,6 +6,36 @@ public:
         int right = n;
         while (left < right) {
             int mid = (left + right) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = (left + right) / 2;
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] > target)
