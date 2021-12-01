8cf616368042a030d4a01294c51762d7ff2c3f2b
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-11-30 20:49:11 -0500

    Update 198_House robber.cpp

--- 198_House robber.cpp
+++ 198_House robber.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev2 = 0;
+        int prev1 = nums[0];
+        for (int i = 1; i < n; ++i) {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return max(prev1, prev2);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
         int len = nums.size();
         if (len == 0)
             return 0;
