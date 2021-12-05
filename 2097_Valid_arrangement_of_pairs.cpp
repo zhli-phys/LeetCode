3d6031a3441c25d116cf0011d80f1eb5dbcf5af7
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-12-05 00:19:22 -0500

    Update 2097_Valid_arrangement_of_pairs.cpp

--- 2097_Valid_arrangement_of_pairs.cpp
+++ 2097_Valid_arrangement_of_pairs.cpp
@@ -35,3 +35,81 @@ public:
         }               
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// struct ListNode {
+//     int val;
+//     ListNode* next;
+    
+//     ListNode(int val) : val(val), next(nullptr) { }
+// };
+
+class Solution {
+public:
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        unordered_map<int, unordered_set<int>> ingoing;
+        unordered_map<int, unordered_set<int>> outgoing;
+        for (auto& pair : pairs) {
+            ingoing[pair[1]].insert(pair[0]);
+            outgoing[pair[0]].insert(pair[1]);
+        }
+        int start = -1;
+        for (auto it = outgoing.begin(); it != outgoing.end(); ++it) {
+            if (ingoing.find(it->first) == ingoing.end() || 
+                it->second.size() > ingoing[it->first].size()) {
+                start = it->first;
+                break;
+            }
+        }
+        if (start < 0) {
+            start = outgoing.begin()->first;
+        }
+        ListNode* head = new ListNode(start);
+        build(outgoing, head);    
+        vector<vector<int>> ans;
+        while (head && head->next) {
+            ans.push_back({head->val, head->next->val});
+            head = head->next;
+        }
+        return ans;
+    }
+    
+    ListNode* build(unordered_map<int, unordered_set<int>>& outgoing, ListNode* curr) {
+        if (outgoing.find(curr->val) == outgoing.end() || outgoing[curr->val].empty())
+            return curr;
+        ListNode* ans = nullptr;
+        while (!outgoing[curr->val].empty()) {
+            auto it = outgoing[curr->val].begin();
+            int num = *it;
+            outgoing[curr->val].erase(it);
+            auto temp = curr->next;
+            curr->next = new ListNode(num);
+            auto tail = build(outgoing, curr->next);
+            if (tail->val == curr->val) {
+                curr = tail;
+                tail->next = temp;
+            }
+            else
+                ans = tail;
+        }
+        if (!ans)
+            ans = curr;
+        return ans;
+    }
+};
