69f19855660b9e96bf474953c457346ffdea86df
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-12-16 21:33:39 -0500

    Update 221_Maximal square.cpp

--- 221_Maximal square.cpp
+++ 221_Maximal square.cpp
@@ -1,6 +1,47 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int maxLen = 0;
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == '1') {
+                dp[0][j] = 1;
+                maxLen = 1;
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            if (matrix[i][0] == '1') {
+                dp[i][0] = 1;
+                maxLen = max(maxLen, 1);
+            }
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    maxLen = max(maxLen, dp[i][j]);
+                }
+            }
+        }
+        return maxLen * maxLen;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
         if (matrix.empty())
             return 0;
         int n = matrix.size();
@@ -34,5 +75,51 @@ public:
             }
         }
         return ans * ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> zeros(m);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '0') {
+                    zeros[i].push_back(j);
+                }
+            }
+            zeros[i].push_back(n);
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '0')
+                    continue;
+                int len = *lower_bound(zeros[i].begin(), zeros[i].end(), j) - j;
+                ans = max(ans, 1);
+                for (int k = i + 1; k < m; ++k) {
+                    if (matrix[i][j] == '0')
+                        break;
+                    len = min(len, *lower_bound(zeros[k].begin(), zeros[k].end(), j) - j);
+                    int side = min(len, (k - i + 1));
+                    ans = max(ans, side * side);
+                }
+            }
+        }
+        return ans;
     }
 };
