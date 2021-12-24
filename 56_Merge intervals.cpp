736b88de563bad4fb5d0c78ee36a9f7033cd31a2
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2021-12-23 19:07:22 -0500

    Update 56_Merge intervals.cpp

--- 56_Merge intervals.cpp
+++ 56_Merge intervals.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int currStart = intervals[0][0];
+        int currEnd = intervals[0][1];
+        vector<vector<int>> ans;
+        for (auto& interval : intervals) {
+            if (interval[0] > currEnd) {
+                ans.push_back({currStart, currEnd});
+                currStart = interval[0];
+                currEnd = interval[1];
+            }
+            else 
+                currEnd = max(currEnd, interval[1]);
+        }
+        ans.push_back({currStart, currEnd});
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int len = intervals.size();
         sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; });
         vector<vector<int>> sol;
