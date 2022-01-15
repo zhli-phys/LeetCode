8d852e9794edcf69d9b3e9cb54ba4211d7e6f7ac
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-01-14 21:41:31 -0500

    Update 253_Meeting rooms.cpp

--- 253_Meeting rooms.cpp
+++ 253_Meeting rooms.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int ans = 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto& interval : intervals) {
+            while (!pq.empty() && pq.top() <= interval[0]) 
+                pq.pop();
+            pq.push(interval[1]);
+            ans = max(ans, static_cast<int>(pq.size()));
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
         if (intervals.empty())
             return 0;
         sort(intervals.begin(), intervals.end());
