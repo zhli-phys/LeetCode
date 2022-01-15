587fe0fa3f47ee7067807c8a14c0348191ea67f1
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-01-15 03:32:51 -0500

    Update 1531_String_compression_II.cpp

--- 1531_String_compression_II.cpp
+++ 1531_String_compression_II.cpp
@@ -2,6 +2,67 @@ class Solution {
 public:
     int getLengthOfOptimalCompression(string s, int k) {
         int n = s.size();
+        int dp[n+1][k+1];
+        for (int i = 0; i <= n; ++i)
+            for (int t = 0; t <= k; ++t)
+                dp[i][t] = 0;
+        int ans = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int t = 1; t <= k; ++t)
+                dp[i][t] = dp[i+1][t-1];
+            int deleted = 0;
+            int num = 0;
+            for (int j = i; j < n; ++j) {
+                if (s[j] != s[i])
+                    ++deleted;
+                else
+                    ++num;
+                for (int t = deleted; t <= k && t < n - i; ++t) {
+                    int currLen = dp[j+1][t-deleted];
+                    if (num == 1)
+                        ++currLen;
+                    else
+                        currLen += 1 + length(num);
+                    if (dp[i][t] == 0 || dp[i][t] > currLen)
+                        dp[i][t] = currLen;
+                }
+            }
+        }
+        return dp[0][k];
+    }
+    
+    inline int length(int num) {
+        if (num < 10)
+            return 1;
+        else if (num < 100)
+            return 2;
+        else
+            return 3;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int getLengthOfOptimalCompression(string s, int k) {
+        int n = s.size();
         vector<vector<int>> dp(n+1, vector<int>(k+1));
         int ans = 0;
         for (int i = n - 1; i >= 0; --i) {
