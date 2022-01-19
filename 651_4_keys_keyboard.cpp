19fd1d7fb6fb68f002fdf025128e1b70971ccb5e
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-01-18 19:33:50 -0500

    Create 651_4_keys_keyboard.cpp

---
+++ 651_4_keys_keyboard.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxA(int n) {
+        if (n <= 3)
+            return n;
+        int dp[n];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        for (int i = 4; i < n; ++i) {
+            int curr = i - 1;
+            for (int j = 3; j <= i / 2; ++j) 
+                curr = max(curr, dp[j] * dp[i-j]);
+            dp[i] = curr;
+        }
+        int ans = n;
+        for (int i = 2; i < n; ++i) 
+            ans = max(ans, i * dp[n-i]);
+        return ans;
+    }
+};
