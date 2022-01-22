7f7bba530b165e33925e3a27a726109681152a72
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-01-22 00:35:30 -0500

    codes

--- 1510_Stone_game_IV.cpp
+++ 1510_Stone_game_IV.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
+        bool dp[n+1];
+        for (int i = 0; i <= n; ++i)
+            dp[i] = false;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                if (!dp[i-j*j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
         if (n == 0) 
             return false;
         unordered_map<int, bool>::iterator it = win.find(n);
