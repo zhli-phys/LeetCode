de477645ad6a5d0725d2ef3f6cd3cfb22d4fe313
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-01-28 22:05:01 -0500

    Update 249_Group_shifted_strings.cpp

--- 249_Group_shifted_strings.cpp
+++ 249_Group_shifted_strings.cpp
@@ -4,6 +4,45 @@ public:
         vector<vector<string>> ans;
         unordered_map<string, int> mp;
         for (string& s : strings) {
+            auto shift_s = shift(s);
+            if (mp.find(shift_s) != mp.end())
+                ans[mp[shift_s]].push_back(s);
+            else {
+                mp[shift_s] = ans.size();
+                ans.push_back({s});
+            }
+        }
+        return ans;
+    }
+    
+    string shift(string s) {
+        int diff = s[0] - 'a';
+        for (char& ch : s) {
+            ch -= diff;
+            if (ch < 'a')
+                ch += 26;
+        }
+        return s;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        vector<vector<string>> ans;
+        unordered_map<string, int> mp;
+        for (string& s : strings) {
             if (mp.find(s) != mp.end())
                 ans[mp[s]].push_back(s);
             else {
