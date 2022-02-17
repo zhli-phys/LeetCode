604e0f53a3d5974ff2facf60b4bcefe6952bbe87
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-02-16 20:18:27 -0500

    Update 39_Combination sum.cpp

--- 39_Combination sum.cpp
+++ 39_Combination sum.cpp
@@ -2,6 +2,51 @@ class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> path(candidates.size());
+        dfs(0, 0, candidates, target, path, ans);
+        return ans;
+    }
+    
+    void dfs(int i, int curr, vector<int>& candidates, int target, vector<int>& path, 
+             vector<vector<int>>& ans) {
+        int n = candidates.size();
+        if (curr == target) {
+            vector<int> sol;
+            for (int j = 0; j < i; ++j) 
+                for (int k = 0; k < path[j]; ++k)
+                    sol.push_back(candidates[j]);
+            ans.push_back(sol);
+        }
+        else if (i >= n || curr + candidates[i] > target)
+            return;
+        else {
+            for (int j = 0; j <= (target - curr) / candidates[i]; ++j) {
+                path[i] = j;
+                dfs(i+1, curr + j * candidates[i], candidates, target, path, ans);
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         return combinationSum_sorted(candidates, target, candidates.size() - 1);
     }
     
