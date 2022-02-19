d225d238818de7c35f1bdf8b1c2f2eb201f2a321
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-02-18 20:53:46 -0500

    Update 1675_Minimize Deviation in Array.cpp

--- 1675_Minimize Deviation in Array.cpp
+++ 1675_Minimize Deviation in Array.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        int minval = INT_MAX;
+        int currMin = INT_MAX;
         priority_queue<int> pq;
         for (int num : nums) {
-            if (num % 2 == 1) {
-                minval = min(minval, 2 * num);
-                pq.push(2 * num);
-            }
-            else {
-                minval = min(minval, num);
-                pq.push(num);
-            }
+            if (num % 2 == 1)
+                num *= 2;
+            currMin = min(currMin, num);
+            pq.push(num);
         }
-        int ans = INT_MAX;
-        while (true) {
-            int curr = pq.top();
-            ans = min(ans, curr - minval);
-            if (curr % 2 == 0) {
-                minval = min(minval, curr / 2);
-                pq.pop();
-                pq.push(curr / 2);
-            }
-            else 
-                return ans;
+        int ans = pq.top() - currMin;
+        while (pq.top() % 2 == 0) {
+            int newNum = pq.top() / 2;
+            pq.pop();
+            currMin = min(currMin, newNum);
+            pq.push(newNum);
+            ans = min(ans, pq.top() - currMin);
         }
+        return ans;
     }
 };
