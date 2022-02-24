4671a3011435af9396711a917e783fb3439c5c3d
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-02-23 20:35:48 -0500

    Update 148_Sort list.cpp

--- 148_Sort list.cpp
+++ 148_Sort list.cpp
@@ -1,6 +1,79 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
+        int n = 0;
+        auto curr = head;
+        while (curr) {
+            ++n;
+            curr = curr->next;
+        }
+        if (n <= 1)
+            return head;
+        return sortList(head, n);
+    }
+    
+    ListNode* sortList(ListNode*& head, int len) {
+        if (len == 1) {
+            auto curr = head;
+            head = head->next;
+            curr->next = nullptr;
+            return curr;
+        }
+        auto left = sortList(head, len / 2);
+        auto right = sortList(head, len - len / 2);
+        return merge(left, right);
+    }
+    
+    ListNode* merge(ListNode* left, ListNode* right) {
+        ListNode* head = nullptr;
+        if (left->val <= right->val) {
+            head = left;
+            left = left->next;
+        }
+        else {
+            head = right;
+            right = right->next;
+        }
+        auto prev = head;
+        prev->next = nullptr;
+        while (left && right) {
+            if (left->val <= right->val) {
+                prev->next = left;
+                left = left->next;
+            }
+            else {
+                prev->next = right;
+                right = right->next;
+            }
+            prev = prev->next;
+        }
+        if (left)
+            prev->next = left;
+        else
+            prev->next = right;
+        return head;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
         if (!head)
             return NULL;
         ListNode* current = head;
