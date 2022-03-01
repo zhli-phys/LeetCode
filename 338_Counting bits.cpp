1b58b859b90d406c0c3bc250aa05fb5aa6816473
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-02-28 19:57:05 -0500

    Update 338_Counting bits.cpp

--- 338_Counting bits.cpp
+++ 338_Counting bits.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        for (int i = 1; i <= n; ++i)
+            ans[i] = ans[i/2] + i % 2;
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
     vector<int> countBits(int num) {
         vector<int> bits(num + 1);
         for (int i = 1; i <= num; i++) {
