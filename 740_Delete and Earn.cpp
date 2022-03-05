e77ddc702f679d8ea78f19d403c506203b54a22a
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-03-05 05:40:46 -0500

    Update 740_Delete and Earn.cpp

--- 740_Delete and Earn.cpp
+++ 740_Delete and Earn.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
+        map<int, int> mp;
+        for (int num : nums)
+            ++mp[num];
+        int m = mp.size();
+        vector<int> nm;
+        for (auto& p : mp)
+            nm.push_back(p.first);
+        vector<int> dp(m+1);
+        dp[1] = mp.begin()->first * mp.begin()->second;
+        for (int i = 2; i <= m; ++i) {
+            if (nm[i-2] == nm[i-1] - 1) {
+                dp[i] = max(nm[i-1] * mp[nm[i-1]] + dp[i-2], dp[i-1]);
+            }
+            else {
+                dp[i] = nm[i-1] * mp[nm[i-1]] + dp[i-1];
+            }
+        }
+        return dp.back();
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
         if (nums.empty())
             return 0;
         unordered_map<int, int> counts;
