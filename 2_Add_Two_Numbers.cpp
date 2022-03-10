842a73415594d047727ca477418c1799c2b6716e
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-03-09 21:11:01 -0500

    Update 2_Add_Two_Numbers.cpp

--- 2_Add_Two_Numbers.cpp
+++ 2_Add_Two_Numbers.cpp
@@ -1,6 +1,46 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode((l1->val + l2->val) % 10);
+        int carry = (l1->val + l2->val) / 10;
+        auto curr = head;
+        l1 = l1->next;
+        l2 = l2->next;
+        while (l1 || l2) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }                
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }                
+            carry = sum / 10;
+            sum %= 10;
+            curr->next = new ListNode(sum);
+            curr = curr->next;
+        }
+        if (carry > 0)
+            curr->next = new ListNode(carry);
+        return head;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
         ListNode* result = new ListNode();
         int temp = l1->val + l2->val;
