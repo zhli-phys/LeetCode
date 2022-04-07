6c633f0a50951c2884a59bf7400b62c4289b7568
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-04-07 03:35:28 -0400

    Create 1046_Last_stone_weight.cpp

---
+++ 1046_Last_stone_weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for (int stone : stones)
+            pq.push(stone);
+        while (pq.size() > 1) {
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            if (x != y)
+                pq.push(y - x);
+        }
+        if (pq.empty())
+            return 0;
+        else
+            return pq.top();
+    }
+};
