db7c12d67ac6d930af6b5ed0912b2c454e76e2ca
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-04-09 00:14:49 -0400

    Update 347_Top K frequent elements.cpp

--- 347_Top K frequent elements.cpp
+++ 347_Top K frequent elements.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for (int num : nums)
+            ++mp[num];
+        vector<pair<int, int>> vec;
+        for (auto& p : mp)
+            vec.push_back({p.second, p.first});
+        sort(vec.begin(), vec.end(), greater<pair<int, int>>());
+        vector<int> ans;
+        for (int i = 0; i < k; ++i) 
+            ans.push_back(vec[i].second);
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> counts;
         for (int i = 0; i < nums.size(); i++) {
             if (counts.find(nums[i]) == counts.end())
