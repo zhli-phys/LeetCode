1998b6e9d8b028837279eef4243206e047467b7d
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-04-09 22:23:01 -0400

    Create 682_Baseball_game.cpp

---
+++ 682_Baseball_game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        vector<int> scores;
+        for (string& op : ops) {
+            if (op == "+") 
+                scores.push_back(scores.back() + scores[scores.size() - 2]);
+            else if (op == "D") 
+                scores.push_back(2 * scores.back());
+            else if (op == "C")
+                scores.pop_back();
+            else
+                scores.push_back(stoi(op));
+        }
+        return accumulate(scores.begin(), scores.end(), 0);
+    }
+};
