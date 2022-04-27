11a2d0696c5ccedd6b03a70e8a453e70a995c49e
Zonghao Li <73005348+zhli-phys@users.noreply.github.com>
2022-04-27 00:55:37 -0400

    Update 1202_Smallest_string_with_swaps.cpp

--- 1202_Smallest_string_with_swaps.cpp
+++ 1202_Smallest_string_with_swaps.cpp
@@ -46,3 +46,63 @@ public:
         return par[i];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        int n = s.size();
+        unordered_map<int, unordered_set<int>> edges;
+        for (auto& pair : pairs) {
+            edges[pair[0]].insert(pair[1]);
+            edges[pair[1]].insert(pair[0]);
+        }
+        
+        vector<bool> seen(n);
+        vector<vector<int>> groups;
+        for (int i = 0; i < n; ++i) {
+            if (!seen[i]) {
+                groups.push_back({});
+                dfs(i, edges, seen, groups.back());
+            }
+        }
+        
+        int m = groups.size();
+        vector<string> strs(m);
+        string ans(n, 0);
+        for (int i = 0; i < m; ++i) {
+            auto& group = groups[i];
+            auto& str = strs[i];
+            int len = group.size();
+            for (int j = 0; j < len; ++j)
+                str.push_back(s[group[j]]);
+            sort(group.begin(), group.end());
+            sort(str.begin(), str.end());
+            for (int j = 0; j < len; ++j)
+                ans[group[j]] = str[j];
+        }
+        return ans;
+    }
+    
+    void dfs(int i, unordered_map<int, unordered_set<int>>& edges, vector<bool>& seen, vector<int>& group) {
+        if (seen[i])
+            return;
+        group.push_back(i);
+        seen[i] = true;
+        for (int next : edges[i])
+            dfs(next, edges, seen, group);
+    }
+};
